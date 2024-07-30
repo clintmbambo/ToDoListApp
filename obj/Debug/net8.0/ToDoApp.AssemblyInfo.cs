2ac0ee1e8c7332a37b954397659b098d52b7fb6c
Clint <clintsiphelelembambo@gmail.com>
2024-07-30 06:58:04 +0200

    front-end changes

--- obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1768de5e360ac424c6238e7286c6cfe1363f2900")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
